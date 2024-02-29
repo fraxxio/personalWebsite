7d8646ae478cbe25058da805dfae351eeb5beb8e
fraxxio <101990229+fraxxio@users.noreply.github.com>
2024-02-29 20:51:09 +0200

    Add interactive cursor effect

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,17 +2,7 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      animation: {
-        fade: "fadeIn 5s ease-in-out",
-      },
-      keyframes: (theme) => ({
-        fadeIn: {
-          "0%": { top: "-10rem" },
-          "100%": { top: "5rem" },
-        },
-      }),
-    },
+    extend: {},
   },
   plugins: [],
 };
