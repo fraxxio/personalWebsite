4ec058f771505ff7e0ba361e00ddfadb8e75f582
fraxxio <101990229+fraxxio@users.noreply.github.com>
2024-02-29 18:46:14 +0200

    Add responsive NavBar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fade: "fadeIn 5s ease-in-out",
+      },
+      keyframes: (theme) => ({
+        fadeIn: {
+          "0%": { top: "-10rem" },
+          "100%": { top: "5rem" },
+        },
+      }),
+    },
   },
   plugins: [],
 };
