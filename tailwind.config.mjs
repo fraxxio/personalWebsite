04ecf591fa9afd058cc9a2aca5018a2d1580d100
fraxxio <101990229+fraxxio@users.noreply.github.com>
2024-03-05 17:35:11 +0200

    Add responsive media queries

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,24 +4,11 @@ export default {
   theme: {
     extend: {
       keyframes: {
-        wave: {
-          "0%": { transform: "rotate(0.0deg)" },
-          "10%": { transform: "rotate(14deg)" },
-          "20%": { transform: "rotate(-8deg)" },
-          "30%": { transform: "rotate(14deg)" },
-          "40%": { transform: "rotate(-4deg)" },
-          "50%": { transform: "rotate(10.0deg)" },
-          "60%": { transform: "rotate(0.0deg)" },
-          "100%": { transform: "rotate(0.0deg)" },
-        },
         hover: {
           "0%": { transform: "scale(1)", filter: "sepia(0%) blur(0px)" },
           "50%": { transform: "scale(1.1)", filter: "sepia(60%) blur(5px)" },
           "100%": { transform: "scale(1)", filter: "sepia(0%) blur(0px)" },
         },
-      },
-      animation: {
-        "waving-hand": "wave 3s linear infinite",
       },
       animation: {
         "hover-project": "hover 700ms ease-in-out",
