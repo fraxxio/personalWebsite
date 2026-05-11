efdf905793cfb1f6250a0813727473bc436e6482
fraxx. <101990229+fraxxio@users.noreply.github.com>
2026-05-11 20:41:13 +0300

    (chore) Upgrade to astro v6

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,9 @@
-import { z, defineCollection } from 'astro:content';
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod'
+import { glob } from 'astro/loaders';
 
 const projectCollection = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/projects" }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
